d02335952dbb85c7b95302c04a47040007cc9724
Bushra-Tabassum11 <130403480+Bushra-Tabassum11@users.noreply.github.com>
2023-08-27 20:11:03 +0200

    Add files via upload

---
+++ data.tf
@@ -0,0 +1,19 @@
+# Terraform Renote State Datasource : This is going to read the data from another statefile
+
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+  config = {
+    bucket = "b54-tf-remote-state"
+    key    = "vpc/${var.ENV}/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+
+data "aws_secretsmanager_secret" "secrets" {
+  name = "robot/secrets"
+}
+
+data "aws_secretsmanager_secret_version" "secret_version" {
+  secret_id = data.aws_secretsmanager_secret.secrets.id
+}
